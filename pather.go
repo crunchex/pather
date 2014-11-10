a53d3d492b019bd400b5eca2a1c34f6c22c82fa7
Mike Lewis <mike@updroid.com>
2014-11-10 15:45:20 -0800

    Added -l flag to return PATH as a long list.

--- pather.go
+++ pather.go
@@ -1,8 +1,21 @@
 package main
 
 import "fmt"
+import "strings"
 import "os"
+import "flag"
 
 func main() {
-	fmt.Printf("%s", os.Getenv("PATH"))
+	useList := flag.Bool("l", false, "use a long listing format")
+
+	flag.Parse()
+
+	if *useList {
+		pathList := strings.Split(os.Getenv("PATH"), ":")
+		for _, p := range pathList {
+			fmt.Println(p)
+		}
+	} else {
+		fmt.Println(os.Getenv("PATH"))
+	}
 }
