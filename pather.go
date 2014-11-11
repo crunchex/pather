a8006cc39de585b71e8fcac23cf29431ce552cd2
Mike Lewis <mike@updroid.com>
2014-11-10 17:17:26 -0800

    Code refactoring.

--- pather.go
+++ pather.go
@@ -1,21 +1,27 @@
 package main
 
-import "fmt"
-import "strings"
-import "os"
-import "flag"
+import (
+	"fmt"
+	"strings"
+	"os"
+	"flag"
+)
+
+func returnPath(shouldList bool) {
+	if !shouldList {
+		fmt.Println(os.Getenv("PATH"))
+		return
+	}
+
+	pathList := strings.Split(os.Getenv("PATH"), ":")
+	for _, p := range pathList {
+		fmt.Println(p)
+	}
+}
 
 func main() {
 	useList := flag.Bool("l", false, "use a long listing format")
 
 	flag.Parse()
-
-	if *useList {
-		pathList := strings.Split(os.Getenv("PATH"), ":")
-		for _, p := range pathList {
-			fmt.Println(p)
-		}
-	} else {
-		fmt.Println(os.Getenv("PATH"))
-	}
+	returnPath(*useList)
 }
